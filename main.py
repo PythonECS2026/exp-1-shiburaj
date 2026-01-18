21a1557a0ff805e3a6b36b1e9de8d5a620ed1754
Shiburaj Pappu <69638955+shiburaj@users.noreply.github.com>
2026-01-18 23:21:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@
 # Write your code here
 name=input("Enter name:")
 age=input("Enter age")
-print("""Welcome to Rizvi College of Engineering\n
+print("""Welcome to Rizvi College of Engineering
 Hello, {name}
 You are {age} years old.""")
